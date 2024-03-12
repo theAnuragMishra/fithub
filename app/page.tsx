730c23d362b55b225af383e54d50de68f93e2197
Anurag <coleridgeprotube@gmail.com>
2024-03-12 13:04:53 +0530

    updated home ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,35 @@ import Image from "next/image";
 import FithubLogo from "@/app/ui/fithub-logo";
 import { lusitana, poppins } from "@/app/ui/fonts";
 import Link from "next/link";
+import { ArrowRightIcon } from "@heroicons/react/16/solid";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-4 pt-10 ">
-      <div
-        className={`bg-blue-500 w-full flex h-20 rounded-lg p-5 md:h-52 items-end shrink-0`}
-      >
+    <main className="flex min-h-screen flex-col items-center p-6">
+      <div className="flex w-full bg-sky-950 h-24 rounded-lg justify-center p-10 md:p-5  mb-10">
         <FithubLogo />
       </div>
-      <div className="flex w-full flex-row justify-center items-center gap-3 px-24">
-        <div className="w-1/3 rounded-md bg-gray-50 h-60 p-5 text-center">
-          <p className={`${lusitana.className} text-xl`}>
+      <div className="flex flex-col justify-center items-center md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg pl-6 py-10 md:w-2/5 md:pl-10 md:ml-20">
+          <p className={`${lusitana.className} text-xl md:text-3xl`}>
             <strong>A fitness and mindfulness hub for everyone!</strong>
+            Fill your life with energy with our daily guided workouts,
+            mindfulness sessions, tracking calendar and{" "}
+            <em>a community leaderboard to keep you going!</em>
           </p>
+          <Link
+            href="/login"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Get Started</span>
+            <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
         </div>
-        <div className="w-2/3 flex justify-center items-center">
+        <div className="flex items-center justify-center p-6  md:px-0 md:py-0">
           <Image
             src="/home-hero.jpg"
             alt="A person doing yoga"
-            width={280}
-            height={280}
+            width={300}
+            height={300}
           />
         </div>
       </div>
--- app/ui/fithub-logo.tsx
+++ app/ui/fithub-logo.tsx
@@ -3,7 +3,7 @@ import { lusitana, poppins } from "@/app/ui/fonts";
 
 export default function FithubLogo() {
   return (
-    <div className="flex flex-row items-center leading-none w-full text-white">
+    <div className="flex flex-row items-center justify-center leading-none w-full text-white ">
       <Image
         className="rounded-full mr-3 invert pb-1"
         src="/logo.svg"
