a94f5d10c12b71e6a308df505565657b00b06446
Anurag <coleridgeprotube@gmail.com>
2024-03-17 02:27:15 +0530

    formatted date to show ist

--- app/ui/home/date.tsx
+++ app/ui/home/date.tsx
@@ -1,17 +1,35 @@
 export default function Today() {
-  var today = new Date();
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  const today = new Date();
 
-  // Get the day, month, and year components
-  var dd = String(today.getDate()).padStart(2, "0");
-  var mm = String(today.getMonth() + 1).padStart(2, "0"); // Jan is 0!
-  var yyyy = today.getFullYear();
+  // Create a new Date object with desired timezone (Asia/Kolkata)
+  const istDate = new Date(
+    today.toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
+  );
+
+  // Get the day, month, and year components from IST date
+  const dd = String(istDate.getDate()).padStart(2, "0");
+  const mm = String(istDate.getMonth() + 1).padStart(2, "0"); // Jan is 0!
+  const yyyy = istDate.getFullYear();
+  const day = istDate.getDay();
 
   // Format the date string
   var formattedDate = dd + "-" + mm + "-" + yyyy;
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="text-6xl">{formattedDate}</h1>
+    <div className="flex flex-row justify-center items-center">
+      <h1 className="text-6xl">
+        {formattedDate} <span className="text-lg">{days[day]}</span>
+      </h1>
     </div>
   );
 }
