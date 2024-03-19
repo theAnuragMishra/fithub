27401bbcf2810018d04f05d653a26d2e182609f0
Anurag <coleridgeprotube@gmail.com>
2024-03-19 11:18:00 +0530

    improved delete account button hover effect

--- app/home/profile/page.tsx
+++ app/home/profile/page.tsx
@@ -41,7 +41,10 @@ export default async function Page() {
           await signOut({ redirectTo: "/" });
         }}
       >
-        <Button variant="outlined" className="bg-red-700 text-white">
+        <Button
+          variant="outlined"
+          className="bg-red-700 text-white hover:bg-red-800"
+        >
           <DeleteOutlineOutlinedIcon />
           Delete Account
         </Button>
